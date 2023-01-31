5fb8e5e3777a124152560008dcbd360497738bf3
seungho-hub <kmc54320@gmail.com>
2023-01-31 13:11:00 +0900

    remove uin encryption

--- src/api/models/Uin.ts
+++ src/api/models/Uin.ts
@@ -2,14 +2,14 @@ import { sequelize } from "./index"
 import { DataTypes } from "sequelize";
 
 const Uin = sequelize.define("uin", {
-    encrypted_uin: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
     user_id: {
         type: DataTypes.UUID,
         allowNull: false,
-    }
+    },
+    uin: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
 }, {
     updatedAt: false,
 })
