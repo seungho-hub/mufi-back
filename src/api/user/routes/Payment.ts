0e112137dc6124c4faaff727528688e46677a4c0
seungho-hub <kmc54320@gmail.com>
2023-02-07 16:37:04 +0900

    remove duplicated file

--- src/api/user/routes/Payment.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express"
-import { getPayment, getPayments } from "../controller/payment"
-export const userPaymentRouter = Router()
-
-userPaymentRouter.get("/:paymentId", getPayment)
-userPaymentRouter.get("/", getPayments)
